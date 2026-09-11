2efac777e9c0d12dc73772e3f686bf82877688bd
shreyasiddheshwar12 <166814221+shreyasiddheshwar12@users.noreply.github.com>
2026-09-11 15:39:27 +0530

    Add additional Terraform variables for configuration
    Added new variables for environment, SQL server, database, app service plan, API name, Application Insights, and storage account.

--- capstone/terraform/variables.tf
+++ capstone/terraform/variables.tf
@@ -10,3 +10,26 @@ variable "sql_admin_password" {
   type      = string
   sensitive = true
 }
+
+
+variable "environment" {}
+
+variable "resource_group_name" {}
+
+variable "location" {}
+
+variable "sql_server_name" {}
+
+variable "sql_database_name" {}
+
+variable "app_service_plan" {}
+
+variable "recall_api_name" {}
+
+variable "appinsights_name" {}
+
+variable "storage_account_name" {}
+
+variable "sql_admin_password" {
+  sensitive = true
+}
