2010676438596ea21dc4fcc25f114ef2832e67bd
Anni2818 <152162121+Anni2818@users.noreply.github.com>
2026-09-11 18:12:29 +0530

    Refactor Azure service plan and web app resources

--- capstone/terraform/modules/appservice/main.tf
+++ capstone/terraform/modules/appservice/main.tf
@@ -1,5 +1,5 @@
-resource "azurerm_service_plan" "plan" {
-  name                = var.plan_name
+resource "azurerm_service_plan" "this" {
+  name                = var.service_plan_name
   resource_group_name = var.resource_group_name
   location            = var.location
 
@@ -7,19 +7,32 @@ resource "azurerm_service_plan" "plan" {
   sku_name = "B1"
 }
 
-resource "azurerm_linux_web_app" "app" {
-  name                = var.app_name
+resource "azurerm_linux_web_app" "this" {
+  name                = var.web_app_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  service_plan_id     = azurerm_service_plan.plan.id
+  service_plan_id     = azurerm_service_plan.this.id
+  https_only          = true
+
+  identity {
+    type = "SystemAssigned"
+  }
 
   site_config {
+    always_on = true
+
     application_stack {
       node_version = "20-lts"
     }
   }
 
   app_settings = {
-    WEBSITE_NODE_DEFAULT_VERSION = "~20"
+    NODE_ENV                  = "production"
+    PORT                      = "8080"
+    SQL_SERVER                = var.sql_server_fqdn
+    SQL_DATABASE              = var.sql_database_name
+    SQL_USER                  = var.sql_admin_login
+    SQL_PASSWORD              = var.sql_admin_password
+    APPLICATIONINSIGHTS_CONNECTION_STRING = var.app_insights_connection_string
   }
 }
