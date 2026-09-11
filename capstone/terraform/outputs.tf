765a23d22726aaa6a7328053a2845bacdf2c8ccf
Anni2818 <152162121+Anni2818@users.noreply.github.com>
2026-09-11 18:15:02 +0530

    Refactor outputs for SQL and API modules

--- capstone/terraform/outputs.tf
+++ capstone/terraform/outputs.tf
@@ -1,15 +1,16 @@
-output "sql_server_name" {
-  value = azurerm_mssql_server.sql.name
-}
-
-output "database_name" {
-  value = azurerm_mssql_database.recalldb.name
-}
-
 output "recall_api_url" {
-  value = azurerm_linux_web_app.recallapi.default_hostname
+  value = "https://${module.recall_api.default_hostname}"
 }
 
-output "application_insights" {
-  value = azurerm_application_insights.insights.name
+output "sql_server_name" {
+  value = module.sql.server_name
 }
+
+output "sql_database_name" {
+  value = module.sql.database_name
+}
+
+output "app_service_principal_id" {
+  value = module.recall_api.principal_id
+}
+``
