b6da819d8b5582ed937f0351f9867fcda8acd7f9
Anni2818 <152162121+Anni2818@users.noreply.github.com>
2026-09-11 18:20:22 +0530

    Create main.tf

---
+++ capstone/terraform/modules/monitoring/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = var.workspace_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  sku               = "PerGB2018"
+  retention_in_days = 30
+}
+
+resource "azurerm_application_insights" "this" {
+  name                = var.application_insights_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.this.id
+}
