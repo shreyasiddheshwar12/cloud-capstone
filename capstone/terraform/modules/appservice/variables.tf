5a7dde68cf580a5fcde18b8be9869daddf213c38
Anni2818 <152162121+Anni2818@users.noreply.github.com>
2026-09-11 18:12:49 +0530

    Add new variables for app service configuration

--- capstone/terraform/modules/appservice/variables.tf
+++ capstone/terraform/modules/appservice/variables.tf
@@ -1,4 +1,37 @@
-variable "plan_name" {}
-variable "app_name" {}
-variable "resource_group_name" {}
-variable "location" {}
+variable "service_plan_name" {
+  type = string
+}
+
+variable "web_app_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "sql_server_fqdn" {
+  type = string
+}
+
+variable "sql_database_name" {
+  type = string
+}
+
+variable "sql_admin_login" {
+  type = string
+}
+
+variable "sql_admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "app_insights_connection_string" {
+  type      = string
+  sensitive = true
+}
