9117d71b6349b53b729147e6b41793d40c5932bd
shreyasiddheshwar12 <166814221+shreyasiddheshwar12@users.noreply.github.com>
2026-09-11 15:52:18 +0530

    Add app_id output and update app_url format

--- capstone/terraform/modules/appservice/outputs.tf
+++ capstone/terraform/modules/appservice/outputs.tf
@@ -1,7 +1,11 @@
 output "app_url" {
-  value = azurerm_linux_web_app.app.default_hostname
+  value = "https://${azurerm_linux_web_app.app.default_hostname}"
 }
 
 output "plan_id" {
   value = azurerm_service_plan.plan.id
+}
+
+output "app_id" {
+  value = azurerm_linux_web_app.app.id
 }
