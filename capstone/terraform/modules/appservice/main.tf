b247fec366bd98260732df535269cafaf5e79875
shreyasiddheshwar12 <166814221+shreyasiddheshwar12@users.noreply.github.com>
2026-09-11 15:45:42 +0530

    Add Azure App Service and Service Plan resources

---
+++ capstone/terraform/modules/appservice/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_service_plan" "plan" {
+  name                = var.plan_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  os_type  = "Linux"
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = var.app_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {}
+}
