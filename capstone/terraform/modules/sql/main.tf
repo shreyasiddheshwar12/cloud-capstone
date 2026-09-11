d61e1763da43cbf6e22f60ba002125f071685fdf
Anni2818 <152162121+Anni2818@users.noreply.github.com>
2026-09-11 18:11:21 +0530

    Refactor SQL resources to use 'this' naming

--- capstone/terraform/modules/sql/main.tf
+++ capstone/terraform/modules/sql/main.tf
@@ -1,22 +1,23 @@
-resource "azurerm_mssql_server" "sql" {
+resource "azurerm_mssql_server" "this" {
   name                         = var.server_name
   resource_group_name          = var.resource_group_name
   location                     = var.location
   version                      = "12.0"
+  minimum_tls_version          = "1.2"
 
   administrator_login          = var.admin_login
   administrator_login_password = var.admin_password
 }
 
-resource "azurerm_mssql_database" "db" {
-  name      = var.db_name
-  server_id = azurerm_mssql_server.sql.id
+resource "azurerm_mssql_database" "this" {
+  name      = var.database_name
+  server_id = azurerm_mssql_server.this.id
   sku_name  = "Basic"
 }
 
-resource "azurerm_mssql_firewall_rule" "allow_azure" {
+resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
   name             = "AllowAzureServices"
-  server_id        = azurerm_mssql_server.sql.id
+  server_id        = azurerm_mssql_server.this.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
 }
